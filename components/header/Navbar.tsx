2e28ddb045d2100cb9f30600cebf1b00de4116b3
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2022-03-29 07:57:52 +0200

    feat{Header}:Added a blur to the background

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <header className="fixed top-2 z-50 flex w-full items-center justify-between px-5">
+    <header className="fixed top-0 z-50 flex w-full items-center justify-between py-2 px-5 backdrop-blur-sm">
       <h1 className="font-moc text-3xl">Organic Bomb</h1>
       <div className="space-y-1">
         <span className="ml-2 block h-0.5 w-8 bg-black "></span>
