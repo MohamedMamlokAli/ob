770d71ad00f71bcdeb2351d2780a19d9fd004d7a
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2022-04-09 09:53:40 +0200

    feat{Navbar}:adjusted the padding and added the base for phone

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import Logo from '../common/Logo'
 
 const Navbar = () => {
   const [offset, setOffset] = useState(0)
+  const [menu, setMenu] = useState(false)
+  const router = useRouter()
+  const toggleMenu = () => {
+    setMenu(!menu)
+  }
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const onScroll = () => setOffset(window.pageYOffset)
@@ -9,18 +16,74 @@ const Navbar = () => {
       return () => window.removeEventListener('scroll', onScroll)
     }
   }, [])
-
+  console.log(router)
   return (
     <header
-      className={`fixed top-0 z-50 flex w-full items-center justify-between py-2 px-5 backdrop-blur-sm transition-colors ${
+      className={`lg:px-15 fixed top-0 z-50 flex w-full items-center justify-between px-10 py-5 backdrop-blur-sm transition-colors lg:py-8 xl:px-20 xl:py-8 ${
         offset > 0 && 'bg-white shadow-md'
       }`}
     >
-      <h1 className="font-moc text-3xl">Organic Bomb</h1>
-      <div className="space-y-1">
+      <Logo />
+      <nav className="hidden w-1/3 md:block">
+        <ul className="flex w-full justify-between  uppercase">
+          <li
+            className={`${router.pathname === '/' && 'border-b border-black'}`}
+          >
+            Home
+          </li>
+          <li
+            className={`${
+              router.pathname === '/blogs' && 'border-b border-black'
+            }`}
+          >
+            Blog
+          </li>
+          <li
+            className={`${
+              router.pathname === '/recipes' && 'border-b border-black'
+            }`}
+          >
+            Recipes
+          </li>
+        </ul>
+      </nav>
+      <div className="z-50 space-y-1" onClick={toggleMenu}>
         <span className="ml-2 block h-0.5 w-8 bg-black "></span>
         <span className="mr-2 block h-0.5 w-8 bg-black"></span>
         <span className="ml-2 block h-0.5 w-8 bg-black "></span>
+      </div>
+      <div
+        id="mobile"
+        className={`absolute inset-0 ${
+          !menu ? 'left-full' : 'left-0'
+        } flex h-screen w-full items-center justify-center bg-red-200  transition-all duration-500 ease-in`}
+      >
+        <nav className=" h-1/2 ">
+          <ul className=" w-full space-y-4  uppercase">
+            <li
+              className={`${
+                router.pathname === '/' && 'border-b border-black'
+              }`}
+            >
+              Home
+            </li>
+            <li
+              className={`${
+                router.pathname === '/blogs' && 'border-b border-black'
+              }`}
+            >
+              Blog
+            </li>
+            <li
+              className={`${
+                router.pathname === '/recipes' && 'border-b border-black'
+              }`}
+            >
+              Recipes
+            </li>
+            <li>search</li>
+          </ul>
+        </nav>
       </div>
     </header>
   )
