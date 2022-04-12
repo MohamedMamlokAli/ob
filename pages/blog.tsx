8195b431c263ce0e6ee4827b1339ac8f17190755
MohamedMamlokAli <mohamedmamlukali@gmail.com>
2022-04-12 08:33:58 +0200

    feat{blogPage}:Removed redundant filter object

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -7,25 +7,6 @@ import { useSelector } from 'react-redux'
 import { selectBlogFilter } from '../store/filter/filterSlice'
 import store from '../store/store'
 const blogs = () => {
-  const filterOptions: Options[] = [
-    {
-      title: 'Category',
-      filters: [
-        'Meat',
-        'Vegetables',
-        'Fruits',
-        'Milk Products',
-        'Legumes',
-        'Smoothies',
-        'Gluten Free',
-        'Vegan',
-      ],
-    },
-    {
-      title: 'Sort',
-      filters: ['Latest', 'Highest Rated'],
-    },
-  ]
   const blogFilters = useSelector(selectBlogFilter)
   console.log(blogFilters)
   return (
